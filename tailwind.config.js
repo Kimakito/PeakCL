22f2fb81067d9b9c5a36ee3a5b8560ecea3308ec
Charlotte <charlotte.lacroix@ikami.fr>
2025-08-12 13:47:18 +0200

    test new visuel

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // Ajout de ma palette de couleurs
-        'primary-blue': '#00bcd4', // Un bleu foncé (comme l'était ton blue-950) pour le fond
-        'secondary-violet': '#5e35b1', // Un violet plus clair
-        'accent-blue': '#1a237e', // Ton turquoise vif
-        'accent-yellow': '#ffc107', // Un jaune doré
-        'gray-light': '#e5e7eb', // Un gris clair pour les textes secondaires
-        'gray-dark': '#374151', // Un gris foncé pour les contrastes
+        // Nouvelle palette de couleurs basée sur le logo PeakCL
+        'peak-blue-light': '#00BDBF', // Le turquoise du logo
+        'peak-blue-dark': '#011526', // Un bleu très foncé, presque noir pour le fond
+        'peak-yellow': '#F9CD00', // Le jaune vif
+        'peak-violet': '#522A80', // Le violet du logo
+        'peak-gray': '#F3F4F6', // Un gris clair pour les textes
+        'peak-gray-dark': '#1F2937', // Un gris foncé pour les contrastes
       },
       // Ajout d'une animation custom pour les dégradés
       animation: {
         'gradient-pulse': 'gradient-pulse 4s ease infinite',
         'fade-in-up': 'fade-in-up 0.6s ease-out forwards',
         'fade-in': 'fade-in 0.6s ease-out forwards',
+        'bg-gradient-shift': 'bg-gradient-shift 10s ease infinite', // Nouvelle animation pour le fond
       },
       // Définition de l'animation
-            keyframes: {
+      keyframes: {
         'gradient-pulse': {
           '0%, 100%': { backgroundPosition: '0% 50%' },
           '50%': { backgroundPosition: '100% 50%' },
@@ -31,7 +32,12 @@ module.exports = {
         'fade-in': {
           '0%': { opacity: 0 },
           '100%': { opacity: 1 },
-        }
+        },
+        'bg-gradient-shift': {
+          '0%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+          '100%': { 'background-position': '0% 50%' },
+        },
       },
     },
   },
